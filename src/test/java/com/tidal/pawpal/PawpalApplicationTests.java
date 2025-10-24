41767ce67a9ce4b576ffb56f3719885c8f6fd6c5
Giovanni Franco <giovannifranco2000@gmail.com>
2025-10-25 00:50:11 +0200

    creazione progetto e aggiunta dipendenze

---
+++ src/test/java/com/tidal/pawpal/PawpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidal.pawpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
