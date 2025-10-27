6ae63d16bef6f2b7b84d06bf037a5c7d42a8997d
Miriam Gallo <95296587+Miriga98@users.noreply.github.com>
2025-10-27 23:40:12 +0100

    con la zuppa di latte poi

--- src/main/java/com/tidal/pawpal/repositories/PrestazioneRepository.java
+++ src/main/java/com/tidal/pawpal/repositories/PrestazioneRepository.java
@@ -3,10 +3,12 @@ package com.tidal.pawpal.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tidal.pawpal.models.Prestazione;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 @Repository
 public interface PrestazioneRepository extends JpaRepository<Prestazione, Long> {
 
-    
-    Prestazione findByRangePrezzo(String rangePrezzo);
+    @Query("SELECT p FROM Prestazione p WHERE p.prezzo BETWEEN ?1 AND ?2")
+   List<Prestazione> findByRangePrezzo(Double prezzoMin, Double prezzoMax);
 
 }
