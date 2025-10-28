7b102a92580943dbb316aa2f6542b9088dacf58c
Miriam Gallo <95296587+Miriga98@users.noreply.github.com>
2025-10-28 15:49:52 +0100

    mi chiamo virgola sono un gattino

--- src/main/java/com/tidal/pawpal/services/RecensioneService.java
+++ src/main/java/com/tidal/pawpal/services/RecensioneService.java
@@ -1,14 +1,37 @@
 package com.tidal.pawpal.services;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tidal.pawpal.models.Cliente;
 import com.tidal.pawpal.models.Recensione;
+import com.tidal.pawpal.models.Veterinario;
+import com.tidal.pawpal.services.contracts.ClienteServiceContract;
+import com.tidal.pawpal.services.contracts.VeterinarioServiceContract;
 import com.tidal.pawpal.services.contracts.RecensioneServiceContract;
 @Service
 public class RecensioneService extends RecensioneServiceContract {
+    @Autowired
+    public RecensioneServiceContract recensioneService;
+    @Autowired
+    public VeterinarioServiceContract veterinarioService;
+    @Autowired
+    public ClienteServiceContract clienteService;
+   
 
+    @Override
+    public Recensione registra (Map<String, String> data) {
+        Recensione recensione = super.registra(data);
+
+    Veterinario v = veterinarioService.cercaPerId(Long.parseLong(data.get("idVeterinario")));
+        recensione.setVeterinario(v);
+    Cliente c = clienteService.cercaPerId(Long.parseLong(data.get("idCliente")));
+        recensione.setCliente(c);
+        return recensione;
+    }   
     @Override
     public List<Recensione> cercaPerVeterinario(Long idVeterinario) {
         // TODO Auto-generated method stub
