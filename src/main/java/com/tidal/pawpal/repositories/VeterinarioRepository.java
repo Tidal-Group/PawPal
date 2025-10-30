4122e8f7787dec05a1146a99a973bc53e3c86c49
Miriam Gallo <95296587+Miriga98@users.noreply.github.com>
2025-10-30 11:34:48 +0100

    espresso macchiato perfavore

--- src/main/java/com/tidal/pawpal/repositories/VeterinarioRepository.java
+++ src/main/java/com/tidal/pawpal/repositories/VeterinarioRepository.java
@@ -24,6 +24,25 @@ public interface VeterinarioRepository extends JpaRepository<Veterinario, Long>,
 
     @Query("SELECT v FROM Veterinario v WHERE LOWER(v.indirizzoStudio) LIKE CONCAT('%', LOWER(:citta), '%')")
     List<Veterinario> findByCitta(String citta);
+    
+   @Query(value = 
+    "WITH SpecializationCounts AS (" +
+    "    SELECT v.specializzazione_nome, COUNT(v.id) AS vet_count " +
+    "    FROM veterinario v " +
+    "    WHERE v.specializzazione_nome IS NOT NULL " +
+    "    GROUP BY v.specializzazione_nome" +
+    ") " +
+    "SELECT sc.specializzazione_nome " +
+    "FROM SpecializationCounts sc " +
+    "ORDER BY sc.vet_count DESC",
+    nativeQuery = true) // <--- Fondamentale per il supporto CTE
+List<String> findPopularSpecializationNamesNative();
+
+@Query("SELECT v " +
+           "FROM Veterinario v JOIN v.recensioni r " +
+           "GROUP BY v " +
+           "ORDER BY AVG(r.voto) DESC, COUNT(r) DESC")
+    List<Veterinario> findTopVeterinariansByAverageRating();
 
     Veterinario findByEmail(String email);
     Veterinario findByTelefono(String telefono);
