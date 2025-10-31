247e7086defedecc8a1134f4cf3d521dd7c6990e
Giovanni Franco <giovannifranco2000@gmail.com>
2025-11-01 00:23:40 +0100

    modifiche routes modals e papagine dashboard

--- src/main/java/com/tidal/pawpal/controllers/AuthController.java
+++ src/main/java/com/tidal/pawpal/controllers/AuthController.java
@@ -1,13 +1,14 @@
 package com.tidal.pawpal.controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -16,10 +17,32 @@ import com.tidal.pawpal.services.AuthService;
 import com.tidal.pawpal.services.PrestazioneService;
 import com.tidal.pawpal.services.SpecieService;
 
-
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+
+    private String redirectToPath(String refererUrl) {
+       String requestPath = "/";
+        try {
+            URI refererUri = new URI(refererUrl);
+            requestPath = refererUri.getPath();
+        } catch (URISyntaxException | NullPointerException exception) {
+
+        }
+        return "redirect:" + requestPath;
+    }
+
+    public String redirectToLoginModal(String refererUrl) {
+       return redirectToPath(refererUrl) + "#loginModal";
+    }
+
+    public String redirectToRegisterClienteModal(String refererUrl) {
+        return redirectToPath(refererUrl) + "#registrazioneClienteModal";
+    }
+
+    public String redirectToRegisterVeterinarioModal(String refererUrl) {
+        return redirectToPath(refererUrl) + "#registrazioneVeterinarioModal";
+    }
 
     @Autowired
     public AuthService authService;
@@ -30,26 +53,9 @@ public class AuthController {
     @Autowired
     public PrestazioneService prestazioneService;
 
-    @GetMapping("/login")
-    public String openModalLogin(Model model) {
-        model.addAttribute("openModal", "auth/login");
-        return "homepage";
-    }
-
-    @GetMapping("/register_veterinario")
-    public String openModalRegisterVeterinario(Model model) {
-        model.addAttribute("openModal", "auth/register_veterinario");
-        return "homepage";
-    }
-
-    @GetMapping("/register_cliente")
-    public String openModalRegisterCliente(Model model) {
-        model.addAttribute("openModal", "auth/register_cliente");
-        return "homepage";
-    }
-
     @PostMapping("/register_veterinario")
     public String sendRegisterVeterinarioData(
+        @RequestHeader(value="Referer", required=false) String refererUrl,
         RedirectAttributes redirectAttributes,
         @RequestParam("specie") List<Long> listaIdSpecie,
         @RequestParam("prestazioni") List<Long> listaIdPrestazioni,
@@ -58,7 +64,7 @@ public class AuthController {
         try {
             authService.registraVeterinario(listaIdSpecie, listaIdPrestazioni, veterinarioData);
             redirectAttributes.addFlashAttribute("successMessage", "Registrazione effettuata con successo");
-            return "redirect:/auth/login";
+            return redirectToLoginModal(refererUrl);
         } catch(Exception exception) {
             // IMPLEMENT CUSTOM ERROR HANDLING
             return "redirect:/error";
@@ -67,13 +73,14 @@ public class AuthController {
 
     @PostMapping("/register_cliente")
     public String sendRegisterClienteData(
+        @RequestHeader(value="Referer", required=false) String refererUrl,
         RedirectAttributes redirectAttributes,
         @RequestParam Map<String, String> clienteData
     ) {
         try {
             authService.registraCliente(clienteData);
             redirectAttributes.addFlashAttribute("successMessage", "Registrazione effettuata con successo");
-            return "redirect:/auth/login";
+            return redirectToLoginModal(refererUrl);
         } catch(Exception exception) {
             // IMPLEMENT CUSTOM ERROR HANDLING
             exception.printStackTrace();
